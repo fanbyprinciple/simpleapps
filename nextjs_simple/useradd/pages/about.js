bc25392c11c0b9d0761da99c6f6722d49e37e238
fanbyprinciple <fanbyprinciple@gmail.com>
2021-03-17 15:44:56 +0530

    added useradd

---
+++ nextjs_simple/useradd/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'next/link';
+const About = () => (
+ <div>
+   <Link href="/">
+     Home
+   </Link>
+   <p>About page</p>
+</div>
+);
+export default About;

61d34d3f40611c6ff0dbaaeac305dcbdb23f48db
fanbyprinciple <fanbyprinciple@gmail.com>
2021-04-15 17:39:43 +0530

    made a small modification

---
+++ me/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import Sidebar from './components/sidebar'
+import Introduction from './components/introduction'
+import About from './components/about'
+import Timeline from './components/timeline'
+import New from './components/new'
+
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        <div id="container-wrap">
+          <New></New>
+         	<Sidebar></Sidebar>
+				<div id="colorlib-main">
+					<Introduction></Introduction>
+					<About></About>
+					<Timeline></Timeline>
+          	</div>
+      	</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ me/src/components/new.jsx
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+export default class New extends Component {
+    render(){
+        return <div><h1> Hello Ashish!</h1></div>
+    }
+}

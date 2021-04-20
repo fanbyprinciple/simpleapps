31ae259592310e81012a16e66dcad1b752c0e45c
fanbyprinciple <fanbyprinciple@gmail.com>
2021-04-20 11:44:03 +0530

    React_SPA: single page applciation added

---
+++ react_spa/src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2> Hello home </h2>
+                <p> Quad Erat Demonstratum</p>
+
+                <p> Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+            </div>
+        )
+    }
+}
+
+export default Home

0d09795eeed06e3467d77522090a13c965c1064e
fanbyprinciple <fanbyprinciple@gmail.com>
2022-01-04 07:30:04 +0530

    tutorial finished

--- splash_page_stopwatch/app/src/main/java/com/example/splash_page_stopwatch/StopWatchAct.java
+++ splash_page_stopwatch/app/src/main/java/com/example/splash_page_stopwatch/StopWatchAct.java
@@ -1,13 +1,65 @@
 package com.example.splash_page_stopwatch;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageView;
 
 public class StopWatchAct extends AppCompatActivity {
 
+    Button btnstart, btnstop;
+    ImageView icanchor;
+    Animation roundingalone;
+    Chronometer timerHere;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
+
+        btnstart = findViewById(R.id.btnstart);
+        btnstop = findViewById(R.id.btnstop);
+        icanchor = findViewById(R.id.icanchor);
+        timerHere = findViewById(R.id.timerHere);
+
+        btnstop.setAlpha(0);
+
+        roundingalone = AnimationUtils.loadAnimation(this, R.anim.roundingalone);
+
+        Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(), "fonts/MRegular.ttf");
+
+        btnstart.setTypeface(MMedium);
+
+        btnstart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                icanchor.startAnimation(roundingalone);
+                btnstop.animate().alpha(1).translationY(-100).setDuration(300).start();
+                btnstart.animate().alpha(0).setDuration(300).start();
+
+                timerHere.setBase(SystemClock.elapsedRealtime());
+                timerHere.start();
+
+            }
+        });
+
+        btnstop.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                  icanchor.clearAnimation();
+                  timerHere.stop(); //pause
+                  btnstop.animate().alpha(0).translationY(100).setDuration(300).start();
+                  btnstart.animate().alpha(1).setDuration(300).start();
+            }
+        });
+
     }
 }
--- splash_page_stopwatch/readme.md
+++ splash_page_stopwatch/readme.md
@@ -11,9 +11,22 @@ Completed splash page design animation
 # Errors currently
 
 1. https://stackoverflow.com/questions/30142056/error-unfortunately-you-cant-have-non-gradle-java-modules-and-android-gradle
+delete .idea folder restart. and also wipe data from emulator and restart emulator.
 
 2. https://stackoverflow.com/questions/23330816/error-package-android-support-v7-app-does-not-exist/57407028
 
+Just needed to remove v7.compat line.
 At 12.22.
 
 Liquidate error first before continuing.
+
+Tutorial finished
+
+![](tut_finished.png)
+# Things to do
+
+1. scale down the arrow of time
+2. look at the background
+3. Change the UI
+4. add a stop and reset button
+5. very important add beep

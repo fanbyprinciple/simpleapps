9c8e0752369227ef3aa1c31bace2a27ffe2bec42
fanbyprinciple <fanbyprinciple@gmail.com>
2022-02-15 00:25:37 +0530

    tried to add summarization

--- chromedriver_bot/whatsapp_alltext_getter.py
+++ chromedriver_bot/whatsapp_alltext_getter.py
@@ -29,6 +29,42 @@ time.sleep(5)
 #message = "Android auto messaging bot here, " +  name + "!"
 
 #names = ["Tanushree"]
+from transformers import pipeline
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def enumerate_link(urlink):    
+    url = urlink
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    #print(text)
+
+
+    split_array = text.split('\n')
+    final = ""
+    for t in split_array:
+        if len(t.split(' '))>4:
+            final += "\n"+ t
+    
+    f = open('file_output', 'w+', encoding='UTF-8')
+
+    f.write(final)
 
 def find_html_in_text(name):
     print("\nsearching for ", name, "\n")
@@ -38,7 +74,7 @@ def find_html_in_text(name):
 
     print("going to top\n")
 
-    days_back = 4
+    days_back = 2
     for i in range(days_back):
         element = driver.find_element_by_xpath("(//div[@class='cvjcv EtBAv'])[2]")
         print(element.text)
@@ -50,14 +86,21 @@ def find_html_in_text(name):
         # actions.move_to_element(element).perform()
 
     print("\nlooking for all messages\n")
-    all_texts = driver.find_elements(By.XPATH,"(//div[@class='_22Msk'])")
+    #all_texts = driver.find_elements(By.XPATH,"(//div[@class='_22Msk'])")
+
+    # lets just search in last element
+    all_texts = [driver.find_element(By.XPATH,"(//div[@class='_22Msk'])[last()]")]
+
 
     # getting https
 
     all_links = []
+
+
     # print(all_texts)
     for t in all_texts:
         a = t.text.split("\n")
+        print(a)
 
         for b in a:
             if "https" in b:
@@ -65,11 +108,28 @@ def find_html_in_text(name):
                 only_link = "https" + b.split('https')[1].split(' ')[0]
                 all_links.append(only_link)
 
+                
+
     all_links = list(set(all_links))
     print(all_links)
+
+
+    enumerate_link(all_links[0])
+
+    f = open('file_output', "r", encoding='utf-8')
+    to_tokenize = str(f.read())
+
+    if len(to_tokenize) > 1024:
+        to_tokenize = to_tokenize[:1024]
+
+    summarizer = pipeline("summarization")
+    summarized = summarizer(to_tokenize, min_length=20, max_length=1024)
+
+    result  = summarized[0]['summary_text']
+
     string_text = '\n'.join(all_links)
 
-    driver.find_element_by_xpath("//div[@title='Type a message']").send_keys(f"Hello! These are all the links exchanged in last {days_back} days.\n {string_text}")
+    driver.find_element_by_xpath("//div[@title='Type a message']").send_keys(f"Hello! one of the links exchanged in last {days_back} days is {all_links[0]}. Here is a short summary of the link : {result}")
     driver.find_element_by_xpath("//div[@title='Type a message']").send_keys(Keys.RETURN)
 
 print("Calling chat_with_a_person().")
